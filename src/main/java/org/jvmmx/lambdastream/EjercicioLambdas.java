98cb05e10fc46dccdc93000cd9f51262faf73cc3
Javier Vega <javier.vega@es.gfi.world>
2020-02-14 09:21:03 +0100

    Mis ejercicios

--- src/main/java/org/jvmmx/lambdastream/EjercicioLambdas.java
+++ src/main/java/org/jvmmx/lambdastream/EjercicioLambdas.java
@@ -1,7 +1,16 @@
 package org.jvmmx.lambdastream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EjercicioLambdas {
 
@@ -14,19 +23,28 @@ public class EjercicioLambdas {
                 "Abraham", "Carlos", "Daniel","Esteban","Francisco","Gustavo","Hugo","Ignacio"
         );
         
-        //Código aquí
-        
+        list.forEach(p->System.out.print(p.charAt(0)));
+        System.out.println();
+        StringBuilder resultado = new StringBuilder();
+        list.forEach(l->resultado.append(l.charAt(0)).append(" "));
+        System.out.println("Resultado: "+resultado);
     }
     
     /**
      * Imprimir sólo las palabras de longitud par
      */
     private void ejercicio2() {
-        List<String> list = Arrays.asList(
+        List<String> list = new ArrayList<>( Arrays.asList(
                 "Abraham", "Carlos", "Daniel","Esteban","Francisco","Gustavo","Hugo","Ignacio"
-        );
+        ));
         
-        //Código aquí
+        list.removeIf(p->p.length()%2!=0);
+        list.forEach(p->System.out.println(p.toString()));
+       
+
+       
+
+        
         
     }
     
@@ -38,8 +56,12 @@ public class EjercicioLambdas {
                 "Abraham", "Carlos", "Daniel","Esteban","Francisco","Gustavo","Hugo","Ignacio"
         );
         
-        //Código aquí
+        StringBuilder res = new StringBuilder();
         
+        list.forEach(p->res.append(p.toUpperCase()).append(" "));
+        String resu=res.toString().trim();
+        System.out.println("longitud de res: "+res.length()+" longitud de resu:"+resu.length());
+        System.out.println("Resultado: " +res.toString().trim());
     }
     
     /**
@@ -48,8 +70,18 @@ public class EjercicioLambdas {
     private void ejercicio4() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         
-        //Código aquí
-        
+        Long a = 500L;
+		(new Thread(() -> list.forEach(System.out::println))).start();
+    }
+    
+    /**
+     * Crear un método que me filtre por palabras pares y lo muestre
+     */
+    private void ejercicio5() {
+    	List<String> list = new ArrayList<>( Arrays.asList(
+                "Abraham", "Carlos", "Daniel","Esteban","Francisco","Gustavo","Hugo","Ignacio"
+        ));
+    	list.stream().filter(l->l.length()%2!=0).forEach(l->System.out.println(l.toUpperCase())); 
     }
     
     public static void main(String[] args) {
@@ -63,9 +95,56 @@ public class EjercicioLambdas {
         el.ejercicio3();
         System.out.println("Ejercicio 4");
         el.ejercicio4();
+        System.out.println("Ejercicio 5");
+        el.ejercicio5();
+        System.out.println();
+        
+        Stream.of("one", "two", "three", "four")
+        .filter(e -> e.length() > 3)
+        .peek(e -> System.out.println("Filtered value: " + e))
+        .map(String::toUpperCase)
+        .peek(e -> System.out.println("Mapped value: " + e))
+        .collect(Collectors.toList());
+        
+        Person lokesh = new Person(1, "Lokesh8", "Kolen");
+        Person brian = new Person(2, "Brian", "Clooney");
+        Person alex = new Person(3, "Alex", "kolen");
+         
+        //Add some random persons
+        List<Person> list = Arrays.asList(lokesh,brian,alex,lokesh,brian,lokesh);
+ 
+        // Get distinct objects by key
+        List<Person> distinctElements = list.stream()
+                            //              .filter( distinctByKey(p -> p.getApellido().toUpperCase()) )
+        					//				.distinct()
+        									.sorted((o1,o2)->o1.getNombre().compareTo(o2.getNombre()))
+                                            .collect( Collectors.toList() );
+ 
+        // Let's verify distinct elements
+        System.out.println( distinctElements );
+        
+        List<Integer> lista = Arrays.asList(-9, -18, 0, 25, 4); 
+        
+        System.out.print("The maximum value is : "); 
+  
+        // Using stream.max() to get maximum 
+        // element according to provided Comparator 
+        // and storing in variable var 
+        int a=3;
+        int var = lista.stream().mapToInt(p->p).filter(p->p>a).sum();
+   //     int var = lista.stream().mapToInt(p->p).filter(p->p>a).sum();
+        System.out.print(var); 
+        
+       
+    
+        
         
     }
+    public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor){
+        Map<Object, Boolean> map = new HashMap<>();
+        return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
 
-    
+
 
 }
--- src/main/java/org/jvmmx/lambdastream/EjercicioStreams.java
+++ src/main/java/org/jvmmx/lambdastream/EjercicioStreams.java
@@ -5,9 +5,40 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class EjercicioStreams {
+	
+	int a;
+	int b;
+	
+	public EjercicioStreams() {	
+	}
+	public EjercicioStreams(int a, int b) {	
+		this.a = a;
+		this.b = b;
+	}
+	void comprobarthis() {
+		System.out.println(this);
+	}
+	static int aa = 3;
+	static int bb;
+	static void meth(int x) {
+	System.out.println("x = " + x);
+	System.out.println("a = " + aa);
+	System.out.println("b = " + bb);
+	}
+	
+	
     
     private static final String PALABRA = "[- .:;,¡!]";
     
@@ -19,7 +50,8 @@ public class EjercicioStreams {
                 Paths.get("poema.txt"), StandardCharsets.UTF_8)){
             
             // código aquí
-            
+        	Integer lineasPoema = (int) reader.lines().count();
+        	System.out.println(lineasPoema);
         }
     }
 
@@ -31,8 +63,40 @@ public class EjercicioStreams {
         try (BufferedReader reader = Files.newBufferedReader(
                 Paths.get("poema.txt"), StandardCharsets.UTF_8)){
             
-            // código aquí
-            
+ //            código aquí
+ //       	List<String> palabras=reader.lines().peek(System.out::println).count();
+//        	Long numero =reader.lines()
+//        		//			.peek(System.out::println)
+//        					.flatMap(l->Stream.of(l.split(PALABRA)))
+//        		//			.peek(System.out::println)
+//        					.map(p->p.toUpperCase().trim())
+//        					.filter(p -> !p.isEmpty())
+//        					.distinct()
+//        					.sorted()
+//        		//			.peek(System.out::println)
+//        					.count();
+//        		//			.peek(System.out::println)
+//        			//		.distinct();
+//        			//		.peek(System.out::println);
+//        					
+//        	System.out.println(numero);
+        	
+        	List<String> palabras = reader.lines()
+        		//	.peek(System.out::println)
+					.flatMap(l->Stream.of(l.split(PALABRA)))
+				//	.peek(System.out::println)
+					.map(p->p.toUpperCase().trim())
+					.filter(p -> !p.isEmpty())
+					.distinct()
+					.sorted()
+				//	.peek(System.out::println)
+					.collect(Collectors.toList());
+					
+					
+       	palabras.stream().forEach(p->System.out.println("palabra :"+p));
+        								
+        								
+   
         }
     }
     
@@ -56,7 +120,14 @@ public class EjercicioStreams {
         try (BufferedReader reader = Files.newBufferedReader(
                 Paths.get("poema.txt"), StandardCharsets.UTF_8)){
             
-            // código aquí
+            List<String> palabrasOrdenadasLongitud = reader.lines()
+            												.flatMap(t->Stream.of(t.split(PALABRA)))
+            												.map(String::trim)
+            												.map(String::toLowerCase)
+            												.distinct()
+            												.sorted((p1,p2)->p2.length()-p1.length())
+            												.peek(System.out::println)
+            												.collect(Collectors.toList());
             
         }
     }
@@ -69,7 +140,35 @@ public class EjercicioStreams {
                 Paths.get("poema.txt"), StandardCharsets.UTF_8)){
             
             // código aquí
-            
+        	Map<String, Long> histograma = new HashMap<String, Long>();
+        	histograma = reader.lines()
+        						.flatMap(linea->Stream.of(linea.split(PALABRA)))
+        						.filter(p->!p.isEmpty())
+        						.map(String::trim)
+        						.map(String::toLowerCase)
+        						.collect(Collectors.groupingBy(
+        								             Function.identity(), 
+        								             Collectors.counting()));
+        	
+        	ArrayList<Entry<String, Long>> list = new ArrayList<>(histograma.entrySet());
+        	Collections.sort(list, valueComparator);
+   //     	Collections.sort(list, Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        	
+        	
+        	for(Entry<String, Long> letra:list) {
+        		System.out.println(letra);
+        	}
+        	
+        	
+        	
+        	
+        	
+//        	
+//        	for (Entry<String, Long> jugador : histograma.entrySet()){
+//        		String clave = jugador.getKey();
+//        		Long valor = jugador.getValue();
+//        		System.out.println(clave+"  ->  "+valor);
+//        	}
         }
     }
     
@@ -78,10 +177,13 @@ public class EjercicioStreams {
      * con una lista de 1000 palabras agrupalas por longitud
      */
     private void ejercicio6() throws IOException {
-        RandomWords rand = new RandomWords();
-        List<String> list = rand.createList(1000);
-        
-        //código aquí
+//        RandomWords rand = new RandomWords();
+//        List<String> list = rand.createList(1000);
+        System.out.println(this);
+//        for(String obj : list) {
+//        	System.out.println(obj);
+//        }
+//      
         
     }
     
@@ -89,18 +191,71 @@ public class EjercicioStreams {
     public static void main(String[] args) throws IOException {
         EjercicioStreams es = new EjercicioStreams();
         System.out.println("Lambdas y Stream API EjercicioStreams");
-        System.out.println("Ejercicio 1");
-        es.ejercicio1();
-        System.out.println("Ejercicio 2");
-        es.ejercicio2();
-        System.out.println("Ejercicio 3");
-        es.ejercicio3();
-        System.out.println("Ejercicio 4");
-        es.ejercicio4();
-        System.out.println("Ejercicio 5");
-        es.ejercicio5();
-        System.out.println("Ejercicio 6");
-        es.ejercicio6();
+//        System.out.println("Ejercicio 1");
+//        es.ejercicio1();
+//        System.out.println("Ejercicio 2");
+//        es.ejercicio2();
+//        System.out.println("Ejercicio 3");
+//        es.ejercicio3();
+//        System.out.println("Ejercicio 4");
+//        es.ejercicio4();
+//        System.out.println("Ejercicio 5");
+//        es.ejercicio5();
+//        System.out.println("Ejercicio 6");
+//        es.ejercicio6();
+        
+        EjercicioStreams ob1 = new EjercicioStreams();
+        ob1.a=1;
+        ob1.b=2;
+        EjercicioStreams ob2 = new EjercicioStreams(4,6);
+        ob2=ob1;
+        ob1.a=3;
+        System.out.println(ob1.a + " " + ob1.b);
+        System.out.println(ob1.hashCode());
+        System.out.println(ob2.a + " " + ob2.b);
+        System.out.println(ob2.hashCode());
+        
+        ob1.comprobarthis();
+        ob2.comprobarthis();
+       
+        EjercicioStreams.bb=3;
+        EjercicioStreams.meth(47);
+       
+        System.out.println("*******************");
+        String strOb1 = "First String";
+        String strOb2 = "Second String";
+        String strOb3 = "First String ";
+        System.out.println("Length of strOb1: " +
+        strOb1.length());
+        System.out.println("Char at index 3 in strOb1: " +
+        strOb1.charAt(3));
+        if(strOb1.equals(strOb2))
+        System.out.println("strOb1 == strOb2");
+        else
+        System.out.println("strOb1 != strOb2");
+        if(strOb1.equals(strOb3))
+        System.out.println("strOb1 == strOb3");
+        else
+        System.out.println("strOb1 != strOb3");
+        
+        System.out.println(strOb1.hashCode());
+        strOb3.trim();
+        System.out.println(strOb3
+        		.hashCode());
+        
     }
+    
+    static {
+    	System.out.println("Static block initialized.");
+    	bb = aa * 4;
+    	}
+    
+    Comparator<Entry<String, Long>> valueComparator = new Comparator<Entry<String, Long>>() {
+
+	    @Override
+	    public int compare(Entry<String, Long> e1, Entry<String, Long> e2) {
+	        return e2.getValue().compareTo(e1.getValue());
+	    }
+	};
     
 }
